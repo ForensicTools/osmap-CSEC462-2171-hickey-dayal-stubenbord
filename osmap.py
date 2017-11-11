bb0db18c497cab3a805056471818fdd932183f8e
Jimmy Hickey <jjhickey96@gmail.com>
2017-11-11 11:45:56 -0500

    Added some comments on how p0f is used in the program

--- osmap.py
+++ osmap.py
@@ -3,35 +3,46 @@ __author__ = "Pranat Dayal, Jimmy Hickey, Ian Stubenbord"
 
 import pyshark
 import sys
-#import subprocess
 import os
 
 def cleanup():
-	os.system("rm osmap.silk; rm osmap.yaf")
+	#remove the files that were created in the running of the program
+	os.system("rm osmap.silk; rm osmap.yaf; rm osmap.p0f")
 	
 
 def generate(pcap):
+	#make a yaf file, a silk file, and make a file from p0f
 	i = "rwp2yaf2silk --in=" + pcap + " --out=osmap.silk"
 	j = "yaf --in " + pcap + " --out osmap.yaf"
+	k = "./p0f -r ../" + pcap + " -o ../osmap.p0f > /dev/null"
 	#subprocess.call(["yaf", i, "--out osmap.yaf"])
 	os.system(i)
 	os.system(j)
+	os.chdir("p0f-3.09b")
+	os.system(k)
+	os.chdir("..")
 
-def analysis(cap):
+def analysis():
 	#this is where we can use pyshark to go through the capture 
 	#provided and see what is in it
-	return 0
+	#now lets use p0f to go through the lines and get the OS detected
+	readFile = open("osmap.p0f", "r")
+	for line in readFile:
+		getOs(line)
+
+def getOs(line):
+	#this is where we can use regex to get the os detected from the line
 
 def main():
 	#print out usage message if file not provided or too many args
 	if len(sys.argv) != 2:
 		print "Usage: osmap <pcap file>"
-	#open up provided file in pyshark and do analysis
+	#open up provided file, create new files and do analysis, then cleanup
 	else:
 		pcap = sys.argv[1]
 		cap = pyshark.FileCapture(sys.argv[1])
-		analysis(cap)
 		generate(pcap)
+		analysis()
 		cleanup()
 
 if __name__=="__main__": main()
