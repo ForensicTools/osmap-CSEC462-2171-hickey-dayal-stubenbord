03055c2e3b6a6e46c10bd3a8789d359f46e76b0d
Jimmy Hickey <jjhickey96@gmail.com>
2017-11-24 11:19:47 -0500

    Changed the method for counting packets to make it more accurate

--- osmap.py
+++ osmap.py
@@ -10,31 +10,20 @@ import re
 ip2os = dict()  # ip key -> Operating system Entry ['192.168.1.1': 'windows 10']
 
 counts = dict()  # ip key -> # of lines IP seen in .p0f ['192.168.1.1': 142]
-#def cleanup():
-	#remove the files that were created in the running of the program
-	#os.system("rm osmap.silk; rm osmap.yaf; rm osmap.p0f")
-#	os.system("rm osmap.silk; rm osmap.yaf")
 
+#def cleanup():
+	#os.system("rm osmap.p0f")
 
 def generate(pcap):
-	#make a yaf file, a silk file, and make a file from p0f
-#	i = "rwp2yaf2silk --in=" + pcap + " --out=osmap.silk"
-#	j = "yaf --in " + pcap + " --out osmap.yaf"
 	k = "./p0f -r ../" + pcap + " -o ../osmap.p0f > /dev/null"
-	#subprocess.call(["yaf", i, "--out osmap.yaf"])
-#	os.system(i)
-#	os.system(j)
 	os.chdir("p0f-JSON")
 	os.system(k)
 	os.chdir("..")
 
 def analysis():
-	#this is where we can use pyshark to go through the capture
-	#provided and see what is in it
 	#now lets use p0f to go through the lines and get the OS detected
 	readFile = open("osmap.p0f", "r")
 	for line in readFile:
-
 		viable = re.search(r'(subj=...\|os=)', line)
 		if viable:
 			type = viable.group(1)[5:8]
@@ -45,9 +34,24 @@ def analysis():
 				ip = re.search(r'(?<=srv=)(.*?)(?=\/)', line).group(1)
 				os = re.search(r'(?<=os=)(.*?)(?=\|)', line).group(1)
 
-			counts[ip] = counts.get(ip, 0) + 1		# counts [ '192.0.0.1' : # of packets ]
+			#counts[ip] = counts.get(ip, 0) + 1		# counts [ '192.0.0.1' : # of packets ]
 			if ip not in ip2os:
 				ip2os[ip] = os
+			elif ip2os[ip] == "???":
+				ip2os[ip] = os
+		index = line.find("subj=")
+ 		if index != -1:
+ 			subInfo = line[index:]
+ 			split = subInfo.partition("|")
+ 			subj = split[0]
+ 			sub = subj[5:]
+			ipIndex = line.find(sub + "=")
+			ipInfo = line[ipIndex:]
+			split = ipInfo.partition("/")
+			ipData = split[0]
+			ip = ipData[4:]
+			counts[ip] = counts.get(ip, 0) + 1		# counts [ '192.0.0.1' : # of packets ]
+
 
 def main():
 	#print out usage message if file not provided or too many args
@@ -66,6 +70,6 @@ def main():
 			for ip in counts:
 				#print("IP: " + ip + "Tally:" + counts[ip] + "OS: " + ip2os[ip])
 				outputWriter.writerow([ip,ip2os[ip],counts[ip]])
-#		cleanup()
+		#cleanup()
 
 if __name__=="__main__": main()
