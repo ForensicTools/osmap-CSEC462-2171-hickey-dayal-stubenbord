1da967550222c80a696404b4acafbba0e6035d45
Pranat Dayal <pranatdayal@blap.local>
2017-12-03 18:58:07 -0500

    legend and count

--- visualize/stats/legend.sh
+++ visualize/stats/legend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # creates legend from csv
 os="$(cat visualize/osmap.csv | cut -d "," -f 2 | sort | uniq | grep -v "category")"
-printf "LIST OF OPERATING SYSTEMS FOUND: \n" > visualize/stats/legend.txt
-printf "$os\n" >> visualize/stats/legend.txt
+grepC="$(grep -c visualize/osmap.csv "$os")"
+printf "$os : $grepC\n" >> visualize/stats/legend.txt
 
