eaac802a8905a5551d041602d0670d8d5efc2d2c
Pranat Dayal <pranatdayal@blap.local>
2017-12-03 18:08:56 -0500

    size

--- visualize/bubble_graph.js
+++ visualize/bubble_graph.js
@@ -55,7 +55,7 @@ function bubbleChart() {
             return +d[columnForRadius];
         }), d3.max(data, function(d) {
             return +d[columnForRadius];
-        })]).range([15,37])     //5,18
+        })]).range([7,37])     //5,18
 
         var node = svg.selectAll("circle")
             .data(data)
