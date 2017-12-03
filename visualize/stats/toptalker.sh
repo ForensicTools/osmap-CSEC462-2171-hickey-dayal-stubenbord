88892b8e70b2c7925a6cee20049807c5218a102e
Pranat Dayal <pranatdayal@blap.local>
2017-12-02 21:40:37 -0500

    fixed directory issues

--- visualize/stats/toptalker.sh
+++ visualize/stats/toptalker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Top talker IP info
 
-for i in $(cat ../osmap.csv | cut -d "," -f 3 | sort -n) ; do echo $i > toptalker.txt;done
+for i in $(cat visualize/osmap.csv | cut -d "," -f 3 | sort -n) ; do echo $i > visualize/stats/toptalker.txt;done
 
-output="$(grep $(cat toptalker.txt) ../osmap.csv)"
+output="$(grep $(cat visualize/stats/toptalker.txt) visualize/osmap.csv)"
 ip="$(echo $output| cut -d"," -f 1)"
 os="$(echo $output| cut -d"," -f 2)"
 conv="$(echo $output| cut -d "," -f 3)"
@@ -11,5 +11,5 @@ conv="$(echo $output| cut -d "," -f 3)"
 
 echo "$(echo "Top Talker:" $ip;
 echo "OS:" $os;
-echo "Conversations:" $conv)" > toptalker.txt
+echo "Conversations:" $conv)" > visualize/stats/toptalker.txt
 
