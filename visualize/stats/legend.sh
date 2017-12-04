edc0cfe47083ee585ed5b042f48d68daa9f5b864
Pranat Dayal <pranatdayal@blap.local>
2017-12-03 20:22:37 -0500

    total count

--- visualize/stats/legend.sh
+++ visualize/stats/legend.sh
@@ -2,5 +2,6 @@
 # creates legend from csv
 os="$(cat visualize/osmap.csv | cut -d "," -f 2 | sort | uniq | grep -v "category")"
 printf "LIST OF OPERATING SYSTEMS: \n" > visualize/stats/legend.txt
+total="$(cat visualize/osmap.csv | wc -l)"
+printf "Total Clients : $total\n" >> visualize/stats/legend.txt
 printf "$os\n" >> visualize/stats/legend.txt
-
